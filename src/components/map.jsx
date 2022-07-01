fded3d1e4a8fcd513972a5bce4bb1351e8953198
Kiril Kaloyanov <kaloyanov.kiril@gmail.com>
2022-07-01 14:10:29 +0300

    Implement routes using GeoJSON format

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,11 +1,7 @@
 import React from "react";
-import {
-  MapContainer,
-  TileLayer,
-  Polyline,
-  Popup,
-  GeoJSON,
-} from "react-leaflet";
+import { MapContainer, TileLayer, Popup, GeoJSON } from "react-leaflet";
+import nat2 from "../data/DunavChernoMore2.json";
+import nat10 from "../data/IronCurtainTrail10.json";
 
 export default function Map() {
   return (
@@ -15,6 +11,12 @@ export default function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        <GeoJSON data={nat2.features} style={{ color: "#3b83d1" }}>
+          <Popup>EuroVelo 6 - Atlantic Ocean - Black Sea</Popup>
+        </GeoJSON>
+        <GeoJSON data={nat10.features} style={{ color: "#d32f2f" }}>
+          <Popup>EuroVelo 13 - Iron Curtain Trail</Popup>
+        </GeoJSON>
       </MapContainer>
     </div>
   );
--- src/components/mapraw.jsx
+++ src/components/mapraw.jsx
@@ -1,27 +1,16 @@
 import React from "react";
-import { MapContainer, TileLayer, Polyline, Popup } from "react-leaflet";
-import mapArrayToRoute from "../data/mapArrayToRoute.js";
-import nat1 from "../data/IskarStruma1";
-import ev13 from "../data/IronCurtainTrail10.js";
-import ev6 from "../data/DunavChernoMore2.js";
-import nat3 from "../data/YantraMaritsa3";
-import nat4 from "../data/Predbalkan4";
-import nat5 from "../data/Ludogorie5";
-import nat6 from "../data/Podbalkanski6";
-import nat7 from "../data/Chernomorski7";
-import nat8 from "../data/ViaDiagonalis8";
+import { MapContainer, TileLayer, GeoJSON, Popup } from "react-leaflet";
+import nat1 from "../data/IskarStruma1.json";
+import nat2 from "../data/DunavChernoMore2.json";
+import nat3 from "../data/YantraMaritsa3.json";
+import nat4 from "../data/Predbalkan4.json";
+import nat5 from "../data/Ludogorie5.json";
+import nat6 from "../data/Podbalkanski6.json";
+import nat7 from "../data/Chernomorski7.json";
+import nat8 from "../data/ViaDiagonalis8.json";
+import nat10 from "../data/IronCurtainTrail10.json";
 
 export default function Map() {
-  let nat1Route = mapArrayToRoute(nat1);
-  let ev6Route = mapArrayToRoute(ev6);
-  let ev13Route = mapArrayToRoute(ev13);
-  let nat3Route = mapArrayToRoute(nat3);
-  let nat4Route = mapArrayToRoute(nat4);
-  let nat5Route = mapArrayToRoute(nat5);
-  let nat6Route = mapArrayToRoute(nat6);
-  let nat7Route = mapArrayToRoute(nat7);
-  let nat8Route = mapArrayToRoute(nat8);
-
   return (
     <div className="map_container">
       <MapContainer center={[42.705, 25.09]} zoom={7.4} scrollWheelZoom={true}>
@@ -29,61 +18,33 @@ export default function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-
-        <Polyline
-          pathOptions={{ color: "#ffeb3b", weight: 5 }}
-          positions={nat1Route}
-        >
+        <GeoJSON data={nat1.features} style={{ color: "#ffeb3b" }}>
           <Popup>Национален маршрут 1 - Искър-Струма</Popup>
-        </Polyline>
-        <Polyline
-          pathOptions={{ color: "#3b83d1", weight: 5 }}
-          positions={ev6Route}
-        >
-          <Popup>EuroVelo 6 - Дунавски велосипеден маршрут</Popup>
-        </Polyline>
-        <Polyline
-          pathOptions={{ color: "#d32f2f", weight: 5 }}
-          positions={ev13Route}
-        >
-          <Popup>EuroVelo 13 - Iron Curtain Trail</Popup>
-        </Polyline>
-        <Polyline
-          pathOptions={{ color: "#827717", weight: 5 }}
-          positions={nat3Route}
-        >
+        </GeoJSON>
+        <GeoJSON data={nat2.features} style={{ color: "#3b83d1" }}>
+          <Popup>EuroVelo 6 - Atlantic Ocean - Black Sea</Popup>
+        </GeoJSON>
+        <GeoJSON data={nat3.features} style={{ color: "#827717" }}>
           <Popup>Национален маршрут 3 - Янтра-Марица-Арда</Popup>
-        </Polyline>
-        <Polyline
-          pathOptions={{ color: "green", weight: 5 }}
-          positions={nat4Route}
-        >
+        </GeoJSON>
+        <GeoJSON data={nat4.features} style={{ color: "green" }}>
           <Popup>Национален маршрут 4 - Предбалкан</Popup>
-        </Polyline>
-        <Polyline
-          pathOptions={{ color: "orange", weight: 5 }}
-          positions={nat5Route}
-        >
+        </GeoJSON>
+        <GeoJSON data={nat5.features} style={{ color: "orange" }}>
           <Popup>Национален маршрут 5 - Лудогорие</Popup>
-        </Polyline>
-        <Polyline
-          pathOptions={{ color: "#7cb342", weight: 5 }}
-          positions={nat6Route}
-        >
+        </GeoJSON>
+        <GeoJSON data={nat6.features} style={{ color: "#7cb342" }}>
           <Popup>Национален маршрут 6 - Подбалкански</Popup>
-        </Polyline>
-        <Polyline
-          pathOptions={{ color: "#2042b3", weight: 5 }}
-          positions={nat7Route}
-        >
+        </GeoJSON>
+        <GeoJSON data={nat7.features} style={{ color: "#2042b3" }}>
           <Popup>Национален маршрут 7 - Черноморски</Popup>
-        </Polyline>
-        <Polyline
-          pathOptions={{ color: "#8e24aa", weight: 5 }}
-          positions={nat8Route}
-        >
+        </GeoJSON>
+        <GeoJSON data={nat8.features} style={{ color: "#8e24aa" }}>
           <Popup>Национален маршрут 8 - Виа Диагоналис</Popup>
-        </Polyline>
+        </GeoJSON>
+        <GeoJSON data={nat10.features} style={{ color: "#d32f2f" }}>
+          <Popup>EuroVelo 13 - Iron Curtain Trail</Popup>
+        </GeoJSON>
       </MapContainer>
     </div>
   );
