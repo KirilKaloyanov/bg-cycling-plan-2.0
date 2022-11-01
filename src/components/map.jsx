50ea8c332c839d36a2c407afed4a2edc41809b15
Kiril <kaloyanov.kiril@gmail.com>
2022-11-01 20:07:45 +0200

    Updated map component

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,33 +1,54 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import { MapContainer, TileLayer, Popup, GeoJSON } from "react-leaflet";
-import dcr from "../data/draft/DunavChernoMore2.json";
-import ict from "../data/draft/IronCurtainTrail10.json";
-import draft from "../data/draft/draft.json";
+import nat1 from "../data/IskarStruma1.json";
+import nat2 from "../data/DunavChernoMore2.json";
+import nat3 from "../data/YantraMaritsa3.json";
+import nat4 from "../data/Predbalkan4.json";
+import nat5 from "../data/Ludogorie5.json";
+import nat6 from "../data/Podbalkanski6.json";
+import nat7 from "../data/Chernomorski7.json";
+import nat8 from "../data/ViaDiagonalis8.json";
+import nat10 from "../data/IronCurtainTrail10.json";
 
 export default function Map() {
   return (
     <>
       <div className="map_container">
-        <MapContainer center={[42.705, 25.09]} zoom={7.4} scrollWheelZoom={false}>
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <GeoJSON data={dcr.features} style={{ color: "#325da8" }}>
-            <Popup>EuroVelo 6 - Atlantic Ocean - Black Sea</Popup>
-          </GeoJSON>
-          <GeoJSON data={ict.features} style={{ color: "#32a852" }}>
-            <Popup>EuroVelo 13 - Iron Curtain Trail</Popup>
-          </GeoJSON>
-          <GeoJSON
-            data={draft.features}
-            style={{ color: "#ed7d40", opacity: 0.5, weight: 10 }}
-          >
-            <Popup>Национален велосипеден маршрут</Popup>
-          </GeoJSON>
-        </MapContainer>
-      </div>
+      <MapContainer center={[42.705, 25.09]} zoom={7.4} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <GeoJSON data={nat1.features} style={{ color: "#ffeb3b" }}>
+          <Popup>Национален маршрут 1 - Искър-Струма</Popup>
+        </GeoJSON>
+        <GeoJSON data={nat2.features} style={{ color: "#3b83d1" }}>
+          <Popup>Национален маршрут 2 - Дунав-Черно море</Popup>
+        </GeoJSON>
+        <GeoJSON data={nat3.features} style={{ color: "#827717" }}>
+          <Popup>Национален маршрут 3 - Янтра-Марица-Арда</Popup>
+        </GeoJSON>
+        <GeoJSON data={nat4.features} style={{ color: "green" }}>
+          <Popup>Национален маршрут 4 - Предбалкан</Popup>
+        </GeoJSON>
+        <GeoJSON data={nat5.features} style={{ color: "orange" }}>
+          <Popup>Национален маршрут 5 - Лудогорие</Popup>
+        </GeoJSON>
+        <GeoJSON data={nat6.features} style={{ color: "#7cb342" }}>
+          <Popup>Национален маршрут 6 - Подбалкански</Popup>
+        </GeoJSON>
+        <GeoJSON data={nat7.features} style={{ color: "#2042b3" }}>
+          <Popup>Национален маршрут 7 - Черноморски</Popup>
+        </GeoJSON>
+        <GeoJSON data={nat8.features} style={{ color: "#8e24aa" }}>
+          <Popup>Национален маршрут 8 - Виа Диагоналис</Popup>
+        </GeoJSON>
+        <GeoJSON data={nat10.features} style={{ color: "#d32f2f" }}>
+          <Popup>Национален маршрут 9 - Пътя на Желязната завеса</Popup>
+        </GeoJSON>
+      </MapContainer>
+    </div>
       <div className="block_offset">
         <h2 className="heading">Концепция за национална мрежа от велосипедни маршрути</h2>
         <div className="text text_column">
