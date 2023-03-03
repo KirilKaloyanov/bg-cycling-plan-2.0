9b1bd15d7678c1dfb46a1a39a9b43a3ff997e678
Kiril <kaloyanov.kiril@gmail.com>
2023-03-03 08:59:00 +0200

    Rebuild with vanilla js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,9 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
+
 const app = express();
-const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, '..', 'build');
 
-app.use(express.static(publicPath));
+app.use(express.static(path.join(__dirname, '..')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(publicPath, 'index.html'));
-});
-
-app.listen(port, () => {
-    console.log(`Server is up on port ${port}...`);
-});
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
